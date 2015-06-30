32b118ff0d1ed179c56abc7363742b9e533540c1
Glen Baker <iepathos@gmail.com>
2015-06-30 05:45:59 -0400

    Comments and code cleanup

--- body/jsx_compile.py
+++ body/jsx_compile.py
@@ -4,21 +4,25 @@ from .config import STATIC_DIR
 
 
 def rename_jsx(jsx_file):
+    """Renames a .jsx filename to .js"""
     return str(jsx_file)[:-3]+'.js'
 
 
 def jsx_filepath(filename):
+    """Returns filepath: static/jsx/filename"""
     return os.path.join('body/static/jsx', filename)
 
 
 def js_filepath(filename):
+    """Returns filepath: static/js/filename"""
     return os.path.join('body/static/js', filename)
 
 
 def jsx_compile():
-    # check files
-    # For multiple paths, use the JSXTransformer class.
+    """Compiles .jsx files in static/jsx into .js files in static/js"""
     transformer = jsx.JSXTransformer()
     jsx_files = os.listdir('body/static/jsx')
     for jsx_file in jsx_files:
-        transformer.transform(jsx_filepath(jsx_file), js_path=js_filepath(rename_jsx(jsx_file)))
+        transformer.transform(
+                jsx_filepath(jsx_file),
+                js_path=js_filepath(rename_jsx(jsx_file)))
